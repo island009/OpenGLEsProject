44ac7d9b89c75a3c8b18634b1757d125c4946402
island <wuxi8112693@163.com>
2017-06-08 08:14:25 +0800

    提交文件

---
+++ OpenGLESProject/RoundPointView.h
@@ -0,0 +1,19 @@
+//
+//  RoundPointView.h
+//  OpenGLESProject
+//
+//  Created by 吴茜 on 2017/6/7.
+//  Copyright © 2017年 island. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <OpenGLES/ES3/gl.h>
+
+@interface RoundPointView : UIView
+
+@end
+@interface RoundPointView(){
+    CAEAGLLayer * glLayer;
+    EAGLContext * context;
+}
+@end
---
+++ OpenGLESProject/RoundPointView.m
@@ -0,0 +1,24 @@
+//
+//  RoundPointView.m
+//  OpenGLESProject
+//
+//  Created by 吴茜 on 2017/6/7.
+//  Copyright © 2017年 island. All rights reserved.
+//
+
+#import "RoundPointView.h"
+
+@implementation RoundPointView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+
+
+@end
+
